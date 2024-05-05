3de0fdc147a44c1137e77fcc7a893c4cad8f263b
bradleyham <bradleyjamesham@gmail.com>
2024-05-05 13:10:01 +1200

    typescript error fix

--- app/SiteComponents/SecondaryButton.tsx
+++ app/SiteComponents/SecondaryButton.tsx
@@ -1,6 +1,6 @@
 import { FaArrowRight } from "react-icons/fa6";
 
-export default function SecondaryButton({text}){
+export default function SecondaryButton({text}: {text: string}) {
 
     return (
         <button className="py-4 italic  font-bold flex items-center space-x-2  "><p className="text-base">{text}</p> <FaArrowRight /></button>
