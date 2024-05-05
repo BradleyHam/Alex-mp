c72f4082cc2d007fbc9a0863972b3c3e8bf9ebc3
bradleyham <bradleyjamesham@gmail.com>
2024-05-05 17:51:22 +1200

    moved hero content up

--- app/HomeComponents/Hero.tsx
+++ app/HomeComponents/Hero.tsx
@@ -7,7 +7,7 @@ export default function Hero(){
         <div className="hero-top-level  h-screen w-screen relative flex flex-col justify-end">
                     <div className="hero-overlay"></div>
                 <div className="container z-10 mx-auto">
-                <div className="hero-text z-20 px-5 pb-[80px]  ">
+                <div className="hero-text z-20 px-5 pb-[120px]  ">
                     <h1 className="hero-title  text-xl lg:text-2xl lg:leading-snug"><strong className="tracking-tight"> Alexander Martin:</strong>
                     <br /><span className="tracking-regular"> A Fresh, Independent Voice Working for a</span> <br /><span className=" span tracking-tight"> Fairer Edinburgh South. </span></h1>
                     <ButtonPrimary text='Get involved'/>
