d570d1c9df761e9cbc75eca4f8987f6e43da6482
bradleyham <bradleyjamesham@gmail.com>
2024-06-30 21:06:29 +1200

    FAQ updates

--- app/HomeComponents/FAQ.js
+++ app/HomeComponents/FAQ.js
@@ -6,7 +6,13 @@ import { FaChevronUp } from 'react-icons/fa';
 const faqData = [
   {
     question: "What impact can I have as an independent politician?",
-    answer: "We are facing either a hung parliament, or slim Labour majority in this general election. Independent politicians and smaller parties will have significant influence by forming alliances on certain issues, and withholding votes."
+    answer: `I will use my free voice and public platform to campaign fearlessly for issues which matter to you. 
+
+    There are many examples of lone politicians making significant change in British political history. 
+    
+    I will bring people together around certain causes, run petitions, organise actions and hold officials to account on your behalf. 
+    
+    I plan to lead all-party initiatives and push for greater transparency on local and national Government spending.`
   },
   {
     question: "Why run as an independent candidate and not under one of the major parties?",
@@ -15,7 +21,23 @@ const faqData = [
   {
     question: "What is my stance on Scottish Independence?",
     answer: "I will ensure that free and fair debate takes place, ensuring that voters can make their own decision about their future as a nation, supporting their right to self-determination."
-  }
+  },
+  {
+    question: "What are my views on sex, gender and equality? ",
+    answer: `I believe in equal rights and responsibilities for all citizens. Regardless of which category they fit into. 
+
+    I believe civil liberties should be defended all the way until the liberty of one individual affects the civil liberties of others. 
+    
+    I believe every minority group which is being unfairly treated, made unsafe or discriminated against should be protected. 
+    
+    These are the core principles from which I approach the current debates on these issues. 
+    
+    Regarding contentious or divisive issues more broadly, I always endeavour to approach the debate from a place of common humanity, empathy and understanding.`
+  },
+  {
+    question: "How are you funding your campaign? ",
+    answer: `My campaign is fully self funded and every one of our 30+ volunteers are entirely unpaid. They are helping me because they believe in what I'm trying to accomplish for Edinburgh South. I am not beholden to any external funding structures or special interest groups.`
+  },
 ];
 
 const FAQ = () => {
